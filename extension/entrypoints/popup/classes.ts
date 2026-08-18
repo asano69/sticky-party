fcda79ed61e8c99969a20c2cf12f8f61d8c6c142
asano69 <186772504+asano69@users.noreply.github.com>
2026-08-18 09:20:34 +0900

    style

--- extension/entrypoints/popup/classes.ts
+++ extension/entrypoints/popup/classes.ts
@@ -5,7 +5,7 @@
 export const CARD = "flex flex-col gap-2 p-3 text-left";
 
 export const FIELD = "flex flex-col gap-0.5";
-export const FIELD_LABEL = "text-xs text-gray-500";
+export const FIELD_LABEL = "text-xs text-[color:var(--note-label)]";
 export const FIELD_INPUT =
   "rounded-md border border-[color:var(--note-button-border)] bg-transparent px-2 py-1 text-[0.9em] text-inherit";
 export const FIELD_TEXTAREA = `${FIELD_INPUT} resize-y font-[inherit]`;
@@ -13,4 +13,4 @@ export const FIELD_TEXTAREA = `${FIELD_INPUT} resize-y font-[inherit]`;
 export const ICON_BTN =
   "inline-flex items-center justify-center rounded-md p-1 text-inherit hover:bg-black/10";
 
-export const SAVED_HINT = "m-0 text-[0.8em] text-gray-500";
+export const SAVED_HINT = "m-0 text-[0.8em] text-[color:var(--note-label)]";
